a3dba5d32a84455e1ec6b8d17b076cebb613696e
johnlimoran110491 <52062740+johnlimoran110491@users.noreply.github.com>
2019-09-03 17:02:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+include('html.php');
+include('js.php');
+$display ='';
+$display= '
+<div class="container" style="margin-top:30px"><div class="row">
+
+           
+
+      
+            </br>
+            </br>
+            </br>
+            <div  class="col-md-9">
+            <h2  style="text-align-last:center;" class="text-info">BETICA SHOP</h2>
+            <select id="product" class="form-control"  style="text-align-last:center; font-size:14px;">
+            </select>
+            </div><!--col-md-12 -->
+            </br>   
+            </br>
+            </br>   
+   
+            <div  class="col-md-9" id="content" style="text-align-last:center; font-size:28px;">
+            </div>
+           
+            
+          
+         
+          </div><!--row -->
+            </div><!--container -->
+            
+          
+            </body>
+</html>'
+            
+            ; 
+            echo $display;
+            
+?>
+
